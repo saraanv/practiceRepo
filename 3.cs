028445680edac4bf5885ebca65215aa5b7285419
saraanv <saraalinezhad1998@gmail.com>
2025-04-02 17:43:52 +0330

    3: a do-while mini practice, this is also the same as the old ones, which calculates just the average, but maybe, more efficient

---
+++ 3.cs
@@ -0,0 +1,24 @@
+﻿//string input = "";
+//do
+//{
+//    Console.WriteLine("Give a number: ");
+//    input = Console.ReadLine();  
+//    Console.WriteLine(input);
+//} while ( input != "0");
+
+
+string input = "";
+int sum = 0;
+int count = 0;
+do
+{
+    Console.WriteLine("Give a number: ");
+    input = Console.ReadLine();
+    sum += int.Parse(input);
+    if (input != "0")
+    {
+        count++;
+    }
+} while (input != "0");
+Console.WriteLine($"avg is {sum / count}");
+
