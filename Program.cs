0c76a36c56f556e97af8fa95f82e1d56f1694ddd
saraanv <saraalinezhad1998@gmail.com>
2025-04-02 16:04:40 +0330

    this is a mini code for calculating the average score of a student

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int sum = 0;
+int count = 0;
+for (int i = 0; i<=10; i++)
+{
+    Console.WriteLine($"your score at {i} lesson (-1 is exit)");
+    string input = Console.ReadLine();
+    int number = int.Parse( input );
+    if (number == -1)
+    {
+        break;
+    }
+    sum += number;
+    count++;
+}
+   int avg;
+    avg = sum / count;
+    Console.WriteLine($"your avg is {avg}");
