b4d317376eea16f0adaa8c206bfccd503a473dc4
saraanv <saraalinezhad1998@gmail.com>
2025-04-04 20:44:36 +0330

    This is a simple game in C#. In which the user should guess the text as many times as they get to the correct one. The the congrats message with the number of attempts is shown.

---
+++ QuessTheTextGame.cs
@@ -0,0 +1,19 @@
+﻿string text = "CSharp";
+int attempts = 0;
+bool found= false;
+Console.WriteLine("Guess a text: ");
+while (!found)
+{
+    string guess = Console.ReadLine();
+    if (guess == text)
+    {
+        attempts++;
+        Console.WriteLine($"You are right! Congratulations! You won after {attempts} attempts.");
+        break;
+    }
+    else
+    {
+        attempts++;
+        Console.WriteLine($"Try again!You had {attempts} attempts");
+    }
+}
