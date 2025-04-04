1e22d0b4499499c6b68800a113a9a9d234459558
saraanv <saraalinezhad1998@gmail.com>
2025-04-04 20:41:24 +0330

    This is a simple game in C#. Which in the user should guess a number, in the user's guess is true, the congrtas massege is shown, if not, the user has only 5 chances to get the right answer, and if they could not, the correct answer is shown.

---
+++ GuessTheNumberGame.cs
@@ -0,0 +1,27 @@
+﻿int Number = 4;
+int ValidInput= 5;
+bool Found = false;
+Console.WriteLine("Guess a number between 1 to 10"); 
+while (!Found)
+{
+    for (int i = 0; i < ValidInput; i++)
+    {
+        string input = Console.ReadLine();
+        int userGuess =  int.Parse(input);
+        if (userGuess == Number )
+        {
+            Console.WriteLine("Right! Congratulations!");
+            Found = true;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong! Try again!");
+        }
+    }
+    if ( !Found )
+    {
+        Console.WriteLine("You tried 5 times! The number is 4!");
+        break;
+    }
+}
