3ef2f6646f5bd07c923565e9a85c1d3210bed1d3
saraanv <saraalinezhad1998@gmail.com>
2025-04-04 20:48:54 +0330

    This code will get a certain n>1 number from user, calculates all the odd numbers before and in the end it gives the sum of those odd numbers

---
+++ SumOfOddNumbersFrom1toN.cs
@@ -0,0 +1,27 @@
+﻿int n;
+int sum = 0;
+bool valid = false;
+Console.WriteLine("Give a number > 1: ");
+string input = Console.ReadLine();
+n = int.Parse(input);
+while (!valid)
+{
+    if (n < 1)
+    {
+        Console.WriteLine("The number should be >1");
+        string input2 = Console.ReadLine();
+        n = int.Parse(input2);
+    }
+    else
+        valid = true;
+}
+for  (int i = 0; i < n; i++)
+{
+    if (i % 2 == 1)
+    {
+        Console.WriteLine($"odd numbers before: {i}");
+        sum = sum + i;
+    }
+
+}
+    Console.WriteLine($"The sum of off numbers before n is = {sum}");
