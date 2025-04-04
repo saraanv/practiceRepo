2fafd17cf8d20f439a77360831e9c31795a28b63
saraanv <saraalinezhad1998@gmail.com>
2025-04-04 20:38:21 +0330

    This is a simple practice over basic concepts of C#. This is a code for practicing over User Inputs and Type Conevrts in C#.

---
+++ TypeConvert_UserInput.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please Enter Your Name: ");
+string sname = Console.ReadLine();
+Console.WriteLine("Please Enter Your Age: ");
+string sage = Console.ReadLine();
+int age = int.Parse(sage);
+Console.WriteLine($"Hello {sname}. You are {age} years old. ");
