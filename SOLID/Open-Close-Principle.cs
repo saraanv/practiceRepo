3ceed9571deb9456ff2bf77d26bedc10cb8c62df
saraanv <saraalinezhad1998@gmail.com>
2025-04-17 15:33:51 +0330

    SOLID: O:Open-Close-Principle= Classes Should Be Opened To Extend And Closeed To Change

---
+++ SOLID/Open-Close-Principle.cs
@@ -0,0 +1,41 @@
+﻿// SOLID: O: Open-Close Principle
+using System.Data;
+
+public class FileExport
+{
+    public void ExportToCSV (string filepath, DataTable data )
+    {
+        // Code To Export Data To CSV File.
+    }
+}
+
+
+// The Code Above Doesn Not Obey SOLID/OCP.
+//What If There Was A Json Export Needed As Well?
+// The Corrected Format:
+
+public abstract class FileExporter
+{
+    public abstract void Export(string filepath, DataTable data);
+}
+public class CsvFileExporter : FileExporter
+{
+    public override void Export(string filepath, DataTable data)
+    {
+        // Code Logic To Export File To CSV File.
+    }
+}
+public class ExcelFileExporter : FileExporter
+{
+    public override void Export(string filepath, DataTable data)
+    {
+        // Code Logic To Export File To Excel File.
+    }
+}
+public class JsonFileExporter : FileExporter
+{
+    public override void Export(string filepath, DataTable data)
+    {
+        // Code Logic To Export File To JSON File.
+    }
+}
