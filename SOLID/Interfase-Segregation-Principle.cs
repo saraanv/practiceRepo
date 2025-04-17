80aa17950ef4266ac0f2c8495829ff3e75aa44ac
saraanv <saraalinezhad1998@gmail.com>
2025-04-17 17:23:19 +0330

    SOLID: I: Interface-Segregation-Principle= An Interface Should Not Have Methods That Class Have To Implement Them Without Any Real Needs.

---
+++ SOLID/Interfase-Segregation-Principle.cs
@@ -0,0 +1,57 @@
+﻿//SOLID: I:Interface Segregation
+public interface IOrder
+{
+    void PlaceOrder();
+    void CancleOrder();
+    void UpdateOrder();
+    void CalculateTotal();
+    void GenerteInvoice();
+    void SendConfirmationEmail();
+    void PrintLabel();
+}
+
+public class OnlineOrder : IOrder
+{
+
+}
+
+public class InStoreOrder : IOrder
+{
+
+}
+
+//The Problem With Code Above Is That An Interface Should Not Contains Constraints Of A Class
+//Have To Implement Them Without Any Real Needs.
+
+//Fixed Code:
+public interface IOrder
+{
+    void PlaceOrder();
+    void CancleOrder();
+    void UpdateOrder();
+}
+public interface IOrderProcessing
+{
+    void CalculateTotal();
+}
+public interface IInvoiceGenerator
+{
+    void GenerateInvoice();
+}
+public interface IEmailSender
+{
+    void SendConfirmationEmail();
+}
+public interface ILabelPrinter
+{
+    void PrintLabel();
+}
+public class OnlineOrder : IOrder , IOrderProcessing , IInvoiceGenerator , IEmailSender
+{
+
+}
+
+public class InStoreOrder : IOrder, IOrderProcessing, ILabelPrinter
+{
+
+}
