1277caa516bdc91bcc3e1a322d3c9305b44cdb1f
saraanv <saraalinezhad1998@gmail.com>
2025-04-18 22:28:36 +0330

    Working with basic concepts on OOP in C#. Here is a Student Class With Name And Age Properties and Fields. A constructor that gets the values from user. and a method to intoduce the student

---
+++ Student-OOP.cs
@@ -0,0 +1,37 @@
+﻿class Student
+{
+    //fields
+    private string name;
+    private int age;
+
+    //proprtis
+    public string Name { get { return name; } set { name = value; } }
+    public int Age { get {return age; } set { age = value; } }
+
+    //method
+    public Student()
+    {
+        Console.WriteLine("Name:");
+        string input1 = Console.ReadLine();
+        name = input1;
+        Console.WriteLine("Age:");
+        string input2 = Console.ReadLine();
+        age = int.Parse(input2);
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hello , my name is {name} and I am {age} years old.");
+    }
+
+}
+
+//Main
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student();
+        student.Introduce();
+    }
+}
