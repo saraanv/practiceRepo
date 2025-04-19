d63b230a0d5f6baf09216c7eaa62a8929894b3d4
saraanv <saraalinezhad1998@gmail.com>
2025-04-19 21:20:34 +0330

    made a banck account with balance/deposit/withdraw/showwbalance. withdraw should subtract the certain amount , unless if not enough, deposit should add to balance the certain amount.

---
+++ BankAccount.cs
@@ -0,0 +1,54 @@
+﻿class BankAccount
+{
+    //Field
+    private int Balance;
+
+    //Constructor
+    public BankAccount(int InitialSet)
+        {
+        Balance = InitialSet;
+        }
+
+
+    //Method Withdraw
+    public void Withdraw(int amount)
+    {
+
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            Console.WriteLine($"New balance with withdraw is {Balance}");
+        }
+        else
+        {
+            Console.WriteLine("Not Enough");
+        }
+    }
+
+    //Method Deposit
+    public void Deposit(int amount)
+    {
+        Balance += amount;
+        Console.WriteLine($"New balance with deposit is {Balance}");
+    }
+
+    public void ShowBalance()
+    {
+        Console.WriteLine($"Balance: {Balance}");
+    }
+
+}
+//Program Class
+class Program
+{
+    //Main
+    static void Main()
+    {
+        BankAccount account = new BankAccount(1000);
+        account.ShowBalance();
+        account.Deposit(300);
+        account.Withdraw(250);
+        account.Withdraw(1500);
+        account.ShowBalance();
+    }
+}
