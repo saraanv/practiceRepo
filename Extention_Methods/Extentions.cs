e7d6b7f9229a7f0d23d2bfb5c2b3282cb32880e2
saraanv <saraalinezhad1998@gmail.com>
2025-05-23 21:26:01 +0330

    Working on Extention methods where you can use a method to prevent redoing something and respecting single priciple on SOLID.

---
+++ Extention_Methods/Extentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Extention
+{
+    public static class Extentions
+    {
+        public static void PutInConsole(this string strings)
+        {
+            Console.WriteLine(strings);
+        }
+        public static string GetPersianDate(this DateTime GregorianDate)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return $"{pc.GetYear(GregorianDate)} / {pc.GetMonth(GregorianDate)} / {pc.GetDayOfMonth(GregorianDate)} / {pc.GetHour(GregorianDate)}:{pc.GetMinute(GregorianDate)}:{pc.GetSecond(GregorianDate)} " ;
+        }
+    }
+}
---
+++ Extention_Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Extention
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            "Hello, world!".PutInConsole();
+            DateTime today = DateTime.Today;
+            Console.WriteLine(today.GetPersianDate());
+            today.GetPersianDate();
+        }
+    }
+}
